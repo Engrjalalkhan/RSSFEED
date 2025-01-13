0a741b57af9d3fa952fc3ee396efb900f87b96af
Jalal khan <jk9494173@gmail.com>
2025-01-13 16:51:01 +0500

    jalal

--- src/screens/RssFeedScreen.js
+++ src/screens/RssFeedScreen.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
 import axios from 'axios';
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   View,
   Text,
@@ -33,70 +33,71 @@ const RssFeedScreen = () => {
   const [saveLoading, setSaveLoading] = useState(false);
   const [lastpage, setLastpage] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [isListView, setIsListView] = useState(true);
   const searchTimeout = useRef(null);
 
 
   const token =
     'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIzIiwianRpIjoiYTg1OGQyMTk2NTk1YzQwZDliYmVhNTdmOWVlMTdkNjkwZTdmZmQ1NDQ3ODM3NmNhOGY4Nzg3ZjQyY2YwMGIzYjc3YTdhZDEzMWM4ZDczMzYiLCJpYXQiOjE3MzU2MzA1MDEuOTIwMTc3LCJuYmYiOjE3MzU2MzA1MDEuOTIwMTgxLCJleHAiOjE3NjcxNjY1MDEuOTAxNDUzLCJzdWIiOiIxNCIsInNjb3BlcyI6W119.vOcLVAKKwSAHm8SaHHyMT3wCIzUYCot-N9yKGD5dJd-FuuHcvbR0syYVQORprbwd7jTgXajaazQsrq5EnMVNL3SamBxN3We56k8Z1bzqaTJ4tSVX4bDkk8cJVtav_y9UjmPOJlyzKh0BdfRJWrA08ySlLAlblKS83lhxSIPkpxxuSHEn4a64IdW6UeCe21D3CicGBMo6GPgea5qpC5DBUBsVihxGjS-aDUBo4_1UFmKtpsJJR7ghQbLlAxOBsx3j2pjfDy5T6I-wyTLn9Md2JIyGQv-vMkvfzBnbDTGwwk3ba3CW9GPWDCFhBuZ-RKL_gIRCebgp4fvATykYV7_tMosjLGlOfPHWxDT5gH9iJtqiiJsW9hBsmQmYQY8yT0GT-Y_dRfVPma6v95Fh3vvVYBXvcFJFySpt4Tprhzlg95BrU7Pc4Fr0YMqXgvr_IKFZBS5wGWxXZqXmiv086DrMaJ_9Fsq-3pjgwX8iyrRKQML7j0Uji4U0vDYzKRTz_nJhVn6zB4Qv9awSHMGGKvXcVBGYVhSzjaajKnx9FLsoxS9e5NmgyHQJ6GPQHFUHv_cjXp6yi_5CbmLZzKeseVngWPGt-Kk0LxCmhJUdjj7r4qVr5NSibZ-6urHi7xcoYOb1NBCrxzh68iVGjvOlXD86QefLCAabocE9oRTrdBm42-s';
 
-    const handleSearch = query => {
-      setSearchQuery(query);
-      if (query.trim() === '') {
-        setSearchLoading(false);
-        setFilterData([]);
-        return;
-      }
+  const handleSearch = query => {
+    setSearchQuery(query);
+    if (query.trim() === '') {
+      setSearchLoading(false);
+      setFilterData([]);
+      return;
+    }
 
-      setSearchLoading(true);
+    setSearchLoading(true);
 
-      if (searchTimeout.current) {
-        clearTimeout(searchTimeout.current);
-      }
+    if (searchTimeout.current) {
+      clearTimeout(searchTimeout.current);
+    }
 
 
-      searchTimeout.current = setTimeout(async () => {
-        try {
-          const response = await axios.get(
-            'http://192.168.18.127:8000/api/rss-feed/search',
-            {
-              params: {search_value: query},
-              headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json',
-              },
+    searchTimeout.current = setTimeout(async () => {
+      try {
+        const response = await axios.get(
+          'http://192.168.18.127:8000/api/rss-feed/search',
+          {
+            params: { search_value: query },
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json',
             },
-          );
+          },
+        );
 
-          if (response?.data?.responseCode === 200) {
-            const feeds = response?.data?.payload?.feeds;
+        if (response?.data?.responseCode === 200) {
+          const feeds = response?.data?.payload?.feeds;
 
-            if (feeds?.length > 0) {
-              const feedsArray = Object.values(feeds);
-              const filteredResults = feedsArray.filter(item => {
-                const titleMatch = item?.title
-                  ?.toLowerCase()
-                  ?.includes(query.toLowerCase());
-                const descriptionMatch = item?.desc
-                  ?.toLowerCase()
-                  ?.includes(query.toLowerCase());
-                return titleMatch || descriptionMatch;
-              });
+          if (feeds?.length > 0) {
+            const feedsArray = Object.values(feeds);
+            const filteredResults = feedsArray.filter(item => {
+              const titleMatch = item?.title
+                ?.toLowerCase()
+                ?.includes(query.toLowerCase());
+              const descriptionMatch = item?.desc
+                ?.toLowerCase()
+                ?.includes(query.toLowerCase());
+              return titleMatch || descriptionMatch;
+            });
 
-              setFilterData(filteredResults);
-            } else {
-              setFilterData([]);
-            }
+            setFilterData(filteredResults);
           } else {
             setFilterData([]);
           }
-        } catch (error) {
-          console.log('Error fetching filtered content:', error.message);
-          setSearchLoading(false);
-        } finally {
-          setSearchLoading(false);
+        } else {
+          setFilterData([]);
         }
-      }, 2500);
-    };
+      } catch (error) {
+        console.log('Error fetching filtered content:', error.message);
+        setSearchLoading(false);
+      } finally {
+        setSearchLoading(false);
+      }
+    }, 2500);
+  };
 
   const RssfeedGetApi = async () => {
     if (currentpage === 1) {
@@ -218,7 +219,7 @@ const RssFeedScreen = () => {
     setData(prevData =>
       prevData.map(item =>
         item.id === id
-          ? {...item, is_selected: !item.is_selected}
+          ? { ...item, is_selected: !item.is_selected }
           : item
       )
     );
@@ -234,6 +235,10 @@ const RssFeedScreen = () => {
   const handleModalClose = () => {
     setSelectedIds([]);
     setModalVisible(false);
+  };
+
+  const toggleViewMode = () => {
+    setIsListView(prevState => !prevState);
   };
 
   return (
@@ -258,7 +263,6 @@ const RssFeedScreen = () => {
       </View>
       <View style={styles.searchbox}>
         <View style={styles.inputContainer}>
-          <Icon name="search" size={20} color="gray" style={styles.icon} />
           <TextInput
             placeholder="Search RSS Feed..."
             placeholderTextColor={'gray'}
@@ -266,24 +270,33 @@ const RssFeedScreen = () => {
             value={searchQuery}
             onChangeText={handleSearch}
           />
+          <Icon name="search" size={20} style={styles.icon} />
         </View>
+        <TouchableOpacity onPress={toggleViewMode}>
+          <Icon
+            name={isListView ? 'grid' : 'list'}
+            size={25}
+            color="red"
+            style={{ paddingLeft: 20, marginTop: 7 }}
+          />
+        </TouchableOpacity>
       </View>
       {/* Conditionally render filtered or full RSS data */}
       {searchLoading ? (
         <ActivityIndicator
           size="large"
           color="#DF4B38"
-          style={{marginTop: 20}}
+          style={{ marginTop: 20 }}
         />
       ) : searchQuery.length > 0 &&
         filterData.length === 0 ? (
         <View style={styles.noResultContainer}>
           <Image
             source={require('../assets/icon/nosearch.png')}
-            style={{width: 30, height: 40}}
+            style={{ width: 30, height: 40 }}
           />
           <Text style={styles.noResultText}>No Record Found</Text>
-          <Text style={{fontSize: 14, color: 'gray'}}>
+          <Text style={{ fontSize: 14, color: 'gray' }}>
             What you searched was unfortunately not found.
           </Text>
         </View>
@@ -295,10 +308,10 @@ const RssFeedScreen = () => {
                 <Icon name="calendar" size={50} color="white" />
               </View>
               <Text style={styles.bodytext}>
-                You have not created any reminder
+                You haven't selected any feed yet.
               </Text>
-              <Text style={{fontSize: 14}}>
-                Created reminders will appear here.
+              <Text style={{ fontSize: 14 }}>
+                Selected RSS Feed will appear here.
               </Text>
             </View>
           </View>
@@ -309,13 +322,17 @@ const RssFeedScreen = () => {
         <FlatList
           data={searchQuery.length === 0 ? rssData : filterData}
           keyExtractor={(item, index) => `${index}-${item.id}`}
-          numColumns={2}
-          renderItem={({item}) => (
+          key={isListView ? 'list' : 'grid'}
+          numColumns={isListView ? 1 : 2}
+          columnWrapperStyle={!isListView && styles.gridContainer}
+          renderItem={({ item }) => (
             <View
               style={[
                 styles.cardContainer,
-                filterData.length === 1 && styles.cardContainerSingle,
-              ]}>
+                isListView ? null : { width: '48%' },
+                filterData.length === 1 && isListView && styles.cardContainerSingle,
+              ]}
+            >
               <TouchableOpacity
                 onPress={() => {
                   if (item.parma_link) {
@@ -323,9 +340,10 @@ const RssFeedScreen = () => {
                       console.error('Error opening link:', err),
                     );
                   }
-                }}>
+                }}
+              >
                 <Image
-                  source={{uri: item.thumbnail}}
+                  source={{ uri: item.thumbnail }}
                   style={styles.thumbnail}
                 />
                 <View style={styles.cardContent}>
@@ -342,7 +360,6 @@ const RssFeedScreen = () => {
               </TouchableOpacity>
             </View>
           )}
-          columnWrapperStyle={styles.columnWrapper}
           onEndReached={handleLoadMore}
           onEndReachedThreshold={0.5}
           ListFooterComponent={
@@ -350,7 +367,7 @@ const RssFeedScreen = () => {
               <ActivityIndicator
                 size="large"
                 color="#DF4B38"
-                style={{margin: 20}}
+                style={{ margin: 20 }}
               />
             )
           }
@@ -363,6 +380,9 @@ const RssFeedScreen = () => {
             />
           }
         />
+
+
+
       )}
 
       {/* Modal with zoom animation */}
@@ -388,7 +408,7 @@ const RssFeedScreen = () => {
               <ActivityIndicator
                 size={'large'}
                 color={'red'}
-                style={{margin: 30}}
+                style={{ margin: 30 }}
               />
             ) : (
               data.map(item => (
@@ -396,17 +416,17 @@ const RssFeedScreen = () => {
                   <TouchableOpacity
                     onPress={() => toggleCategorySelection(item.id)}
                     style={[
-                      {height: 40, borderRadius: 5},
+                      { height: 40, borderRadius: 5 },
                       selectedIds.includes(item.id) || item.is_selected
-                        ? {backgroundColor: '#DF4B38'}
-                        : {backgroundColor: '#D8D8D8'},
+                        ? { backgroundColor: '#DF4B38' }
+                        : { backgroundColor: '#D8D8D8' },
                     ]}>
                     <Text
                       style={[
                         styles.categoriestext,
                         selectedIds.includes(item.id) || item.is_selected
-                          ? {color: 'white'}
-                          : {color: 'black'},
+                          ? { color: 'white' }
+                          : { color: 'black' },
                       ]}>
                       {item.feed_name}
                     </Text>
@@ -484,13 +504,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: 'lightgray',
-    borderRadius: 20,
-    width: '70%',
+    borderRadius: 10,
+    width: '90%',
     position: 'relative',
   },
   icon: {
     position: 'absolute',
-    left: 10,
+    right: 10,
     color: '#DF4B38',
   },
   input: {
@@ -498,6 +518,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingLeft: 40,
     color: 'black',
+    right: 20,
   },
   defaultContainer: {
     paddingTop: 20,
@@ -587,19 +608,19 @@ const styles = StyleSheet.create({
   },
   gridContainer: {
     flexDirection: 'row',
-    flexWrap: 'wrap',
     justifyContent: 'space-between',
+    marginHorizontal: 10,
   },
   cardContainer: {
-    margin: 3,
+    margin: 5,
     borderRadius: 8,
     backgroundColor: '#fff',
     elevation: 2,
     shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowRadius: 4,
-    shadowOffset: {width: 0, height: 2},
-    width: '48%',
+    shadowOffset: { width: 0, height: 2 },
+    width: '97%',
   },
   thumbnail: {
     width: '100%',
@@ -632,7 +653,7 @@ const styles = StyleSheet.create({
   },
   cardContainerSingle: {
     margin: 10,
-    width: 180,
+    width: '95%',
     marginRight: 180,
   },
   noResultContainer: {
